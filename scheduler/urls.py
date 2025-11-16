65946235df925376c4e36255b189e44a8b1e5722
Vvk_0105 <vivekbabu.sics@gmail.com>
2025-11-16 14:27:44 +0530

    Add task control endpoints for pausing, resuming, and executing tasks manually

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -7,4 +7,11 @@ urlpatterns = [
     path('edit-task/<int:task_id>/', views.edit_task),
     path('delete-task/<int:task_id>/', views.delete_task),
 
+    path('task/<int:task_id>/pause/', views.pause_task),
+    path('task/<int:task_id>/resume/', views.resume_task),
+
+    path('task/<int:task_id>/run-now/', views.run_now),
+
+    path('cron-preview/', views.cron_preview),
+
 ]
--- scheduler/views.py
+++ scheduler/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Task
+from .models import Task, TaskExecutionHistory
 from .utils import get_next_run_time
+from croniter import croniter
+from datetime import datetime
 
 # Create your views here.
 
@@ -46,3 +48,49 @@ def edit_task(request, task_id):
 def delete_task(request, task_id):
     Task.objects.get(id=task_id).delete()
     return Response({"message": "Task deleted"})
+
+@api_view(['POST'])
+def pause_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.is_paused = True
+    task.save()
+    return Response({"message": "Task paused"})
+
+
+@api_view(['POST'])
+def resume_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.is_paused = False
+    task.save()
+    return Response({"message": "Task resumed"})
+
+@api_view(['POST'])
+def run_now(request, task_id):
+    task = Task.objects.get(id=task_id)
+
+    TaskExecutionHistory.objects.create(
+        task=task,
+        status="manual",
+        log="Task executed manually"
+    )
+
+    task.next_run_at = get_next_run_time(task.cron_expression)
+    task.save()
+
+    return Response({"message": "Task executed manually"})
+
+@api_view(['POST'])
+def cron_preview(request):
+    cron = request.data.get("cron_expression")
+
+    if not cron:
+        return Response({"error": "cron_expression required"}, status=400)
+
+    now = datetime.now()
+    itr = croniter(cron, now)
+
+    next_times = []
+    for _ in range(5):
+        next_times.append(itr.get_next(datetime).strftime("%Y-%m-%d %H:%M:%S"))
+
+    return Response({"next_runs": next_times})
